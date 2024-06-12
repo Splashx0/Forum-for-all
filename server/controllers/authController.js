8199c1837ff620c794d3a3d3b68d06b1cb9b8c8e
splash <splashs089@gmail.com>
2024-06-13 00:10:01 +0100

    fixing issues so i can deploy to vercel

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,9 @@
-const User = require("../models/user");
-const jwt = require("jsonwebtoken");
+import { register as _register, login as _login } from "../models/user";
+import { sign } from "jsonwebtoken";
 
 const maxAge = 3 * 24 * 60 * 60;
 const createToken = (_id, username) => {
-  return jwt.sign({ _id, username }, process.env.SECRET, {
+  return sign({ _id, username }, process.env.SECRET, {
     expiresIn: maxAge,
   });
 };
@@ -11,7 +11,7 @@ const createToken = (_id, username) => {
 const register = async (req, res) => {
   const { username, email, password } = req.body;
   try {
-    const user = await User.register(username, email, password);
+    const user = await _register(username, email, password);
     const token = createToken(user._id, user.username);
     res.status(200).json({ username: user.username, token });
   } catch (error) {
@@ -22,7 +22,7 @@ const register = async (req, res) => {
 const login = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const user = await User.login(email, password);
+    const user = await _login(email, password);
     const token = createToken(user._id, user.username);
     res.status(200).json({ username: user.username, token });
   } catch (error) {
@@ -30,4 +30,4 @@ const login = async (req, res) => {
   }
 };
 
-module.exports = { register, login };
+export default { register, login };
