8b55671c33e48a94644d871a9f59f8e9937c1e58
splash <splashs089@gmail.com>
2024-06-13 21:42:58 +0100

    replacing require with import

--- server/routes/topicRoutes.js
+++ server/routes/topicRoutes.js
@@ -4,6 +4,6 @@ import  { getTopics, createTopic }  from "../controllers/topicController.js";
 const router = Router();
 
 router.get("/", getTopics);
-router.post("/", createTopic);
+router.post("/", createTopic); 
 
 export default router;
