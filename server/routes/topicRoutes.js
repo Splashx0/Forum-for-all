5f7fd7a72986f2627c805d77ea11ddb613df7fe5
splash <splashs089@gmail.com>
2024-06-13 23:49:10 +0100

    Bugs

--- server/routes/topicRoutes.js
+++
@@ -1,9 +0,0 @@
-import {Router} from "express"
-import  { getTopics, createTopic }  from "../controllers/topicController.js";
-
-const router = Router();
-
-router.get("/", getTopics);
-router.post("/", createTopic); 
-
-export default router;
